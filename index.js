7175ca9facb644caa2a4e803581dd0200283f271
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-05-10 20:25:42 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+
+// SDK de Mercado Pago
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
+const client = new MercadoPagoConfig({
+  accessToken: "TEST-2179645305992199-051017-849c70f7cc3a9335f93fa9a7f6c04cef-510224380",
+});
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/create_preference", async (req, res) => {
+  try {
+    const idempotencyKey = req.headers["x-idempotency-key"];
+    console.log(idempotencyKey)
+
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.unit_price),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: "https://utopiastw.com/",
+        failure: "https://utopiastw.com/",
+        pending: "https://utopiastw.com/",
+      },
+      auto_return: "approved",
+    };
+
+    console.log(body);
+
+    const preference = new Preference(client);
+    const result = await preference.create({ body, idempotencyKey });
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+        error: "Error al crear la preferencia"
+    })
+  }
+});
+
+app.listen(port, () => {
+  console.log(`El servidor esta corriendo en el puerto ${port}`);
+});
