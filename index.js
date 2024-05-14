c22b5d27bf2eaaff0696895fea6d7c77e41a1b83
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-05-14 16:14:14 -0300

    cors  config

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://utopiastw.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
